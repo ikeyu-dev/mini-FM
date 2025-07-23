6ddbb3355b0207fac68f3d67c04fcde1ab81e49d
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-07-23 11:55:24 +0900

    一覧表示のUIを改善

--- src/components/ProgramList.vue
+++ src/components/ProgramList.vue
@@ -46,27 +46,36 @@ console.log(data.value);
             v-for="program in data.programs"
             class="carousel-item p-5"
         >
-            <div class="card w-full bg-base-100 shadow-xs">
-                <figure>
+            <div
+                class="grid grid-cols-3 gap-4 items-center bg-base-100 shadow-sm w-full p-4"
+            >
+                <div class="col-span-1">
                     <img
                         :src="program.imgSrc ?? ''"
-                        alt="program image"
-                        class="h-10"
+                        class="w-full rounded"
                     />
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title">{{ program.title }}</h2>
-                    <div class="card-actions justify-end">
+                </div>
+                <div class="col-span-2">
+                    <h2 class="text-sm md:text-xl font-bold">
+                        {{ program.title }}
+                    </h2>
+                    <p class="text-xs md:text-sm text-gray-600">
+                        再生ボタンを押すと再生ページに移動します
+                    </p>
+                    <nuxt-link
+                        :to="program.link ?? ''"
+                        class="flex items-center justify-end mt-2"
+                    >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 640 640"
-                            class="h-6 w-6 cursor-pointer"
+                            class="h-10 w-10 cursor-pointer"
                         >
                             <path
                                 d="M64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM252.3 211.1C244.7 215.3 240 223.4 240 232L240 408C240 416.7 244.7 424.7 252.3 428.9C259.9 433.1 269.1 433 276.6 428.4L420.6 340.4C427.7 336 432.1 328.3 432.1 319.9C432.1 311.5 427.7 303.8 420.6 299.4L276.6 211.4C269.2 206.9 259.9 206.7 252.3 210.9z"
                             />
                         </svg>
-                    </div>
+                    </nuxt-link>
                 </div>
             </div>
         </div>

a7f67dd6d1442cc5bf38fb1dae1b45d89237346d
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-07-27 15:31:30 +0900

    iframe使用に伴うスクレイピング周りの修正

--- src/server/api/programs.ts
+++ src/server/api/programs.ts
@@ -14,13 +14,10 @@ export default defineEventHandler(async () => {
             const extractedPrograms = elements.map((element) => {
                 const titleElement = element.querySelector('[jsname="r4nke"]');
                 const linkElement = element.querySelector("a");
-                const imgElement = element.querySelector("img");
                 const title = titleElement ? titleElement.textContent : null;
                 const link = linkElement ? linkElement.href : null;
-                // const imgSrc = imgElement ? imgElement.src : null;
                 return { title, link };
             });
-            // Add an extra program with empty title, link, and imgSrc
             extractedPrograms.push({
                 title: "none",
                 link: "none",

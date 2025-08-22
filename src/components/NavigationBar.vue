d9e7089a0210307c971222eb9231824c880d0b94
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-08-22 14:26:38 +0900

    style: navbarのUIを改善

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -29,11 +29,13 @@ onUnmounted(() => {
         </div>
         <section class="navbar-end">
             <!-- TODO: md以上ではテキスト表示を行う(レスポンシブ対応) -->
-            <ul class="menu menu-horizontal bg-base-200 rounded-box md:gap-4">
+            <ul
+                class="menu menu-horizontal rounded-box p-0 md:gap-4 fill-white text-white font-bold"
+            >
                 <li>
                     <nuxt-link
                         to="/"
-                        class="tooltip tooltip-bottom"
+                        :class="{ 'tooltip tooltip-bottom': !md_show }"
                         data-tip="Home"
                     >
                         <svg
@@ -41,6 +43,7 @@ onUnmounted(() => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 640 640"
                             class="h-4 md:h-6 w-4 md:w-6"
+                            data-tip="Home"
                         >
                             <path
                                 d="M304 70.1C313.1 61.9 326.9 61.9 336 70.1L568 278.1C577.9 286.9 578.7 302.1 569.8 312C560.9 321.9 545.8 322.7 535.9 313.8L527.9 306.6L527.9 511.9C527.9 547.2 499.2 575.9 463.9 575.9L175.9 575.9C140.6 575.9 111.9 547.2 111.9 511.9L111.9 306.6L103.9 313.8C94 322.6 78.9 321.8 70 312C61.1 302.2 62 287 71.8 278.1L304 70.1zM320 120.2L160 263.7L160 512C160 520.8 167.2 528 176 528L224 528L224 424C224 384.2 256.2 352 296 352L344 352C383.8 352 416 384.2 416 424L416 528L464 528C472.8 528 480 520.8 480 512L480 263.7L320 120.3zM272 528L368 528L368 424C368 410.7 357.3 400 344 400L296 400C282.7 400 272 410.7 272 424L272 528z"
@@ -48,7 +51,7 @@ onUnmounted(() => {
                         </svg>
                         <div
                             v-if="md_show"
-                            class="hidden md:inline"
+                            class="hidden md:inline hover:scale-125 transition-transform duration-300"
                         >
                             Home
                         </div>
@@ -57,7 +60,7 @@ onUnmounted(() => {
                 <li>
                     <nuxt-link
                         to="/letter"
-                        class="tooltip tooltip-bottom"
+                        :class="{ 'tooltip tooltip-bottom': !md_show }"
                         data-tip="お便り"
                     >
                         <svg
@@ -72,7 +75,7 @@ onUnmounted(() => {
                         </svg>
                         <div
                             v-if="md_show"
-                            class="hidden md:inline"
+                            class="hidden md:inline hover:scale-125 transition-transform duration-300"
                         >
                             お便り
                         </div>
@@ -81,7 +84,7 @@ onUnmounted(() => {
                 <li>
                     <nuxt-link
                         to="/episodes"
-                        class="tooltip tooltip-bottom"
+                        :class="{ 'tooltip tooltip-bottom': !md_show }"
                         data-tip="過去の放送回"
                     >
                         <svg
@@ -96,7 +99,7 @@ onUnmounted(() => {
                         </svg>
                         <div
                             v-if="md_show"
-                            class="hidden md:inline"
+                            class="hidden md:inline hover:scale-125 transition-transform duration-300"
                         >
                             過去の放送回
                         </div>
@@ -105,7 +108,7 @@ onUnmounted(() => {
                 <li>
                     <nuxt-link
                         to="#"
-                        class="tooltip tooltip-bottom"
+                        :class="{ 'tooltip tooltip-bottom': !md_show }"
                         data-tip="お知らせ"
                     >
                         <svg
@@ -120,7 +123,7 @@ onUnmounted(() => {
                         </svg>
                         <div
                             v-if="md_show"
-                            class="hidden md:inline"
+                            class="hidden md:inline hover:scale-125 transition-transform duration-300"
                         >
                             お知らせ
                         </div>
